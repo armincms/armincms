f1cd22688ad98f0400a67b82a63293a0df81c81a
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-09-28 15:47:41 +0330

    Restricts the user to update self roles

--- src/Nova/Role.php
+++ src/Nova/Role.php
@@ -3,6 +3,7 @@
 namespace Armincms\Nova;
  
 use Zareismail\NovaPolicy\Nova\Role as Resource;
+use Illuminate\Http\Request;
 
 class Role extends Resource
 {    
@@ -14,4 +15,26 @@ class Role extends Resource
      * @var string
      */
     public static $group = 'ACL';
+
+    /**
+     * Determine if the current user can view the given resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $ability
+     * @return bool
+     */
+    public function authorizedTo(Request $request, $ability)
+    { 
+    	if($request->user()->isDeveloper()) {
+    		return true;
+    	}
+
+    	return with(parent::authorizedTo($request, $ability), function($authorizedTo) use ($request, $ability) {
+    		if(! in_array($ability, ['viewAny', 'view']) && $authorizedTo) { 
+    			return $request->user()->roles()->whereKey($this->resource->getKey())->count() === 0;
+    		}
+
+    		return $authorizedTo;
+    	}); 
+    }
 }
