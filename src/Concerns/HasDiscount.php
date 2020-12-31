f019815377678d70fe0ffc3b64d4b34d6942b2a3
Ismail Zare <zarehesmaiel@gmail.com>
2020-12-31 15:57:50 +0330

    Fixes division by zero

--- src/Concerns/HasDiscount.php
+++ src/Concerns/HasDiscount.php
@@ -21,7 +21,9 @@ trait HasDiscount
      */
     public function discountPercent(float $amount)
     { 
-    	return  ($amount - $this->applyDiscount($amount)) / $amount * 100; 
+        if($amount === 0) return 100;
+
+    	return ($amount - $this->applyDiscount($amount)) / $amount * 100; 
     } 
 
     /**
